84b13587b1ce3905f20ab2ddc244fe18d5eecbb8
unknown <lam@cphbusiness.dk>
2021-04-25 09:44:46 +0200

    ready as start for exercise

---
+++ src/schema.ts
@@ -0,0 +1,30 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import { resolvers } from './resolvers';
+
+const typeDefs = `
+
+type Coordinate {
+  latitude: Float!
+  longitude:Float!
+}
+
+type Coordinates {
+  coordinates: [Coordinate]
+}
+
+type Status {
+  status:String
+  msg: String
+
+}
+
+type Query {
+
+  """Returns a GeoJson Polygon representing the legal gameArea"""
+  gameArea : Coordinates 
+}
+`;
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export { schema };
